1a77fe10be5915f3c5b3f2f6a09d12d4bf3b5a58
Itz__prakash_910 <160889786+Itzprakash910@users.noreply.github.com>
2026-07-08 07:39:50 +0530

    Add files via upload

---
+++ frontend/sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'presethub-v1';
+const urlsToCache = [
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/manifest.json',
+  '/assets/icons/icon-72.png',
+  '/assets/icons/icon-96.png',
+  '/assets/icons/icon-128.png',
+  '/assets/icons/icon-144.png',
+  '/assets/icons/icon-152.png',
+  '/assets/icons/icon-192.png',
+  '/assets/icons/icon-256.png',
+  '/assets/icons/icon-384.png',
+  '/assets/icons/icon-512.png',
+  '/assets/icons/apple-touch-icon.png',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700;800&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css'
+];
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
